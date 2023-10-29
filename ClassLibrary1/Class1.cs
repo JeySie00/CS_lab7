bec94fd63c6dcd20829b06e4b8108bb14567da45
JeySie00 <86301125+JeySie00@users.noreply.github.com>
2023-10-29 17:32:46 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,125 @@
+﻿namespace ClassLibrary1;
+[Comment("Abstract class for creating animals")] 
+
+abstract public class Animal
+{
+    public string Country { get; set; }
+    public string HideFromOtherAnimals { get; set; }
+    public string Name { get; set; }
+    public string WhatAnimal{ get; set; }
+    eClassificationAnimal Classification {  get; set; }
+    protected eFavoriteFood FavoriteFood { get; set; }
+
+    public Animal(string country, string hideFromOtherAnimals, string name, string whatAnimal)
+    {
+        Country = country;
+        HideFromOtherAnimals = hideFromOtherAnimals;
+        Name = name;
+        WhatAnimal = whatAnimal;
+    }
+
+    public void Deconstruct(out string country, out string hideFromOtherAnimals, out string name, out string whatAnimal)
+    {
+        country = Country;
+        hideFromOtherAnimals = HideFromOtherAnimals;
+        name = Name;
+        whatAnimal = WhatAnimal;
+    }
+
+    public eClassificationAnimal GetClassificationAnimal
+    {
+        get { return Classification; }
+    }
+
+    public virtual eFavoriteFood GetFavoriteFood
+    {
+        get { return FavoriteFood; }
+    }
+
+    public virtual void SayHello()
+    {
+        Console.WriteLine($"Hello, i'm {Name}");
+    }
+}
+
+[Comment("Class for cow")]
+public class Cow : Animal
+{
+    public Cow(string country, string hideFromOtherAnimals, string name, string whatAnimal) : base(country, hideFromOtherAnimals, name, whatAnimal)
+    {
+        Country = country; 
+        HideFromOtherAnimals= hideFromOtherAnimals;
+        Name = name;
+        WhatAnimal = whatAnimal;
+    }
+
+    public override eFavoriteFood GetFavoriteFood
+    {
+        get { return FavoriteFood; }
+    }
+
+    public override void SayHello()
+    {
+        Console.WriteLine($"Hello, i'm cow {Name}");
+    }
+}
+
+[Comment("Class for lion")]
+public class Lion : Animal
+{
+    public Lion(string country, string hideFromOtherAnimals, string name, string whatAnimal) : base(country, hideFromOtherAnimals, name, whatAnimal)
+    {
+        Country = country;
+        HideFromOtherAnimals = hideFromOtherAnimals;
+        Name = name;
+        WhatAnimal = whatAnimal;
+    }
+
+    public override eFavoriteFood GetFavoriteFood
+    {
+        get { return FavoriteFood; }
+    }
+
+    public override void SayHello()
+    {
+        Console.WriteLine($"Hello, i'm lion {Name}");
+    }
+}
+
+[Comment("Class for pig")]
+public class Pig : Animal
+{
+    public Pig(string country, string hideFromOtherAnimals, string name, string whatAnimal) : base(country, hideFromOtherAnimals, name, whatAnimal)
+    {
+        Country = country;
+        HideFromOtherAnimals = hideFromOtherAnimals;
+        Name = name;
+        WhatAnimal = whatAnimal;
+    }
+
+    public override eFavoriteFood GetFavoriteFood
+    {
+        get { return FavoriteFood; }
+    }
+
+    public override void SayHello()
+    {
+        Console.WriteLine($"Hello, i'm pig {Name}");
+    }
+}
+
+[Comment("Types of animals enumeration")]
+public enum eClassificationAnimal
+{
+    Herbivores,
+    Carnivores,
+    Omnivores
+}
+
+[Comment("Favorite food enumeration")]
+public enum eFavoriteFood
+{
+    Meat,
+    Plants,
+    Everything
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+using System.Xml;
+using ClassLibrary1;
+
+class Programm
+{
+    static void Main()
+    {
+        XmlDocument xmlDocument = new XmlDocument();
+        XmlElement rootElement = xmlDocument.CreateElement("Diagram");
+        xmlDocument.AppendChild(rootElement);
+
+        Assembly assembly = Assembly.Load("ClassLibrary1");
+        Type[] types = assembly.GetTypes();
+
+        foreach (Type type in types)
+        {
+            string temp;
+            if (type.IsClass)
+            {
+                temp = "Class";
+            }
+            else
+            {
+                temp = "Enum";
+            }
+
+            if (type.Namespace.Contains("ClassLibrary1"))
+            {
+                XmlElement element = xmlDocument.CreateElement(temp);
+                rootElement.AppendChild(element);
+
+                element.SetAttribute("name", type.Name);
+
+                CommentAttribute comment = (CommentAttribute)type.GetCustomAttribute(typeof(CommentAttribute));
+                if (comment != null)
+                {
+                    XmlElement commentElement = xmlDocument.CreateElement("Comment");
+                    commentElement.InnerText = comment.Comment;
+                    element.AppendChild(commentElement);
+                }
+
+                object[] properties = type.GetProperties();
+                foreach (var prop in properties)
+                {
+                    XmlElement propertyElement = xmlDocument.CreateElement("Property");
+                    propertyElement.InnerText = prop.ToString();
+                    element.AppendChild(propertyElement);
+                }
+
+                object[] methods = type.GetMethods(BindingFlags.DeclaredOnly);
+                foreach (var method in methods)
+                {
+                    XmlElement methodElement = xmlDocument.CreateElement("Method");
+                    methodElement.InnerText = method.ToString();
+                    element.AppendChild(methodElement);
+                }
+            }
+
+            
+        }
+
+        xmlDocument.Save("/Users/user/source/repos/lab7/ConsoleApp/ClassDiagram.xml");
+    }
+}
